a5237c865363f613c3840cf8ba6d7e7256f6da4d
DennyAhn <aa5051140@gmail.com>
2024-05-16 17:50:48 +0900

    Your commit message

--- app/src/main/java/com/flius/guidy/RegistrationActivity.kt
+++ app/src/main/java/com/flius/guidy/RegistrationActivity.kt
@@ -23,6 +23,9 @@ class RegistrationActivity : AppCompatActivity() {
         // BottomNavigationView 변수 선언 및 초기화
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
+        // 프로필 추가 Button 변수 선언 및 초기화
+        val buttonIcon: ImageButton = findViewById(R.id.baseline_add)
+
         // BottomNavigationView의 아이템 선택 리스너 설정
         bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
@@ -47,16 +50,17 @@ class RegistrationActivity : AppCompatActivity() {
             }
         }
 
-        // ImageButton 변수 선언 및 초기화
-        val buttonIcon: ImageButton = findViewById(R.id.baseline_add)
         buttonIcon.setOnClickListener {
-            setContentView(R.layout.activity_register_post)
+            val intent = Intent(this, Register_post::class.java)
+            startActivity(intent)
+
         }
     }
 
     private fun getPersonList(): List<Person> {
         return listOf(
-            Person("좁밥준상ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ", "Details about John"),
+            Person("이름: 좁밥준상" ,
+                    "나이: 23"+ "   성별: 남자"),
             Person("멸치민재", "Details about Jane"),
             Person("뿡뿡이기성", "Details about Sam"),
             // 추가 인물 데이터
@@ -67,15 +71,16 @@ class RegistrationActivity : AppCompatActivity() {
         AlertDialog.Builder(this).apply {
             setTitle("로그아웃 확인")
             setMessage("정말로 로그아웃 하시겠습니까?")
+            setNegativeButton("아니오") { dialog, _ ->
+                dialog.dismiss()
+            }
             setPositiveButton("예") { _, _ ->
                 // 로그인 화면으로 이동
                 val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(intent)
             }
-            setNegativeButton("아니오") { dialog, _ ->
-                dialog.dismiss()
-            }
+
             create()
             show()
         }
