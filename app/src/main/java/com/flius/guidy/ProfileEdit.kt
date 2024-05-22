a1fbeac6bb42c278209eb4217bbaa8a580844dd0
DennyAhn <aa5051140@gmail.com>
2024-05-22 23:00:57 +0900

    커밋 메시지

--- app/src/main/java/com/flius/guidy/ProfileEdit.kt
+++ app/src/main/java/com/flius/guidy/ProfileEdit.kt
@@ -1,19 +1,43 @@
 package com.flius.guidy
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class ProfileEdit : AppCompatActivity() {
+
+    private lateinit var profileImageView: ImageView
+    private val PICK_IMAGE_REQUEST = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_edit)
+
+        profileImageView = findViewById(R.id.profile_image)
+        profileImageView.setOnClickListener {
+            openGallery()
+        }
 
         val saveButton: Button = findViewById(R.id.profile_save_button)
         saveButton.setOnClickListener {
             val intent = Intent(this, RegistrationActivity::class.java)
             startActivity(intent)
+        }
+    }
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+            val imageUri: Uri? = data.data
+            profileImageView.setImageURI(imageUri)
         }
     }
 }
